2c498e063856058e8cc51d37bfb1e970aa770f91
Xurape <xarope1337@gmail.com>
2022-11-06 22:49:02 +0000

    Fixed config file

--- src/Providers/ReduxPanelServiceProvider.php
+++ src/Providers/ReduxPanelServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types = 1);
+declare (strict_types = 1);
 
 namespace Xuap\ReduxPanelLib\Providers;
 
@@ -9,17 +9,17 @@ use Xuap\ReduxPanelLib\Console\InstallReduxPanelLib;
 
 class ReduxPanelServiceProvider extends ServiceProvider
 {
-  public function register()
-  {
-    //
-  }
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'reduxpanel');
+    }
 
-  public function boot()
-  {
-      if ($this->app->runningInConsole()) {
-          $this->commands([
-              InstallReduxPanelLib::class,
-          ]);
-      }
-  }
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                InstallReduxPanelLib::class,
+            ]);
+        }
+    }
 }
