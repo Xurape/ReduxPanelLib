0f23754199fbfa8c0bd7179e24b3f2d5cd62fa15
Xurape <xarope1337@gmail.com>
2022-11-06 22:59:03 +0000

    Added comments for better readibility

--- src/config/config.php
+++ src/config/config.php
@@ -1,6 +1,27 @@
 <?php
 
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Default URL for the ReduxPanel
+    |--------------------------------------------------------------------------
+    |
+    | This is the panel url which will allow the library
+    | to communicate with your ReduxPanel service.
+    |
+    */
     'URL' => "",
+
+    /*
+    |--------------------------------------------------------------------------
+    | API Key generated
+    |--------------------------------------------------------------------------
+    |
+    | Go to:
+    | -> https://<your_panel_link>/modulos
+    | -> Check "Informações"
+    | -> Copy the "API KEY" and place if below
+    |
+    */
     'API_KEY' => ""
 ];
