65a46897cf6c7878babfc3c75aa6146f2a707179
Xurape <xarope1337@gmail.com>
2022-11-08 17:45:24 +0000

    Added some data verification

--- src/Facades/ReduxPanel.php
+++ src/Facades/ReduxPanel.php
@@ -18,10 +18,21 @@ class ReduxPanel extends Facade
         return Http::get(config('reduxpanel.URL') . '/modules/api/data?comando=versao')['versao'];
     }
 
-    public static function getPlayerInfo($steamid)
+    public static function getPlayerInfo($steamid = null)
     {
+        if($steamid == null)
+            return "Please fill out all data.";
+
         $id = ReduxPanelUtils::convertSteamID($steamid);
 
         return Http::get(config('reduxpanel.URL') . "/modules/api/data?comando=info_jogador&api_key=".config('reduxpanel.API_KEY')."&id=$id");
+    }
+    
+    public static function addServer($nome = null, $ip = null, $porta = null, $rcon = null)
+    {
+        if($nome == null || $ip == null || $porta == null || $rcon == null)
+            return "Please fill out all data.";
+
+        return Http::get(config('reduxpanel.URL') . "/modules/api/data?comando=addservidor&api_key=".config('reduxpanel.API_KEY')."&nome=$nome&ip_numerico=$ip&porta=$porta&rcon=$rcon");
     }
 }
