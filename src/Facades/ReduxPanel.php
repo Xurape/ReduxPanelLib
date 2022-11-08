df247c14a098900cd6d94638ca09dbdb7734060d
Xurape <xarope1337@gmail.com>
2022-11-08 16:56:05 +0000

    Added getplayerinfo function

--- src/Facades/ReduxPanel.php
+++ src/Facades/ReduxPanel.php
@@ -4,6 +4,7 @@ namespace Xuap\ReduxPanelLib\Facades;
 
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Facade;
+use Xuap\ReduxPanelLib\Utils\ReduxPanelUtils;
 
 class ReduxPanel extends Facade
 {
@@ -15,5 +16,12 @@ class ReduxPanel extends Facade
     public static function getPanelVersion()
     {
         return Http::get(config('reduxpanel.URL') . '/modules/api/data?comando=versao')['versao'];
+    }
+
+    public static function getPlayerInfo($steamid)
+    {
+        $id = ReduxPanelUtils::convertSteamID($steamid);
+
+        return Http::get(config('reduxpanel.URL') . "/modules/api/data?comando=info_jogador&api_key=".config('reduxpanel.API_KEY')."&id=$id");
     }
 }
